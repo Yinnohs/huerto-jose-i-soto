862a9c9c932322c86d5bbd0ace4e2c69cad63183
josesoto <josesoto@itop.es>
2023-01-16 19:20:58 +0000

    refactor(CropWorker): updated cropw worke type cliento to a separated class

--- src/main/java/org/example/domain/Client.java
+++ src/main/java/org/example/domain/Client.java
@@ -1,2 +1,19 @@
-package org.example.domain;public class Client {
+package org.example.domain;
+
+public class Client extends  Thread{
+    private String name;
+    private int quantityToConsume;
+
+    private  Orchard orchard;
+
+
+
+
+    @Override
+    public  void  run(){
+        for (int i = 0; i < quantityToConsume ; i++) {
+            System.out.println("The Client: " + this.name + " is consuming: a crop");
+            this.orchard.consumeVegetable(this.name);
+        }
+    }
 }
