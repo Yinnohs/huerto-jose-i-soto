933889d54197bfd3739b6277c59dfdaa73d2780a
josesoto <josesoto@itop.es>
2023-01-16 19:25:17 +0000

    refactor(CropWorker):deleted functionality

--- src/main/java/org/example/domain/CropWorker.java
+++ src/main/java/org/example/domain/CropWorker.java
@@ -17,23 +17,10 @@ public class CropWorker extends Thread  {
         this.orchard = orchard;
     }
 
+    // TODO: spread functionality to single class.
+    // i don't need
     @Override
     public  void run(){
-        if(this.type == CropWorkerType.CLIENT){
-            for (String crop : crops){
-                System.out.println("The Client: " + this.name + " is consuming: " + crop);
-                this.orchard.consumeVegetable(crop);
-            }
-        }
-        else if (this.type == CropWorkerType.FARMER) {
-            for (String crop : crops){
-                System.out.println("The Farmer: " + this.name + " is planting: " + crop);
-                this.orchard.produceVegetable(crop);
-            }
-        }
-        else {
-            System.err.println("This type of Perticipant does not exist");
-        }
     }
 
 
